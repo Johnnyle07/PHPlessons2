0d1501dbce338e8b51fcd51b11f6c89cdb3b51da
Johnnyle07 <155994933+Johnnyle07@users.noreply.github.com>
2025-03-27 09:48:50 -0500

    phplessons2

--- index.php
+++ index.php
@@ -7,15 +7,17 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
+    
     <form method="GET">
       <input  type="text" name="person">
       <button> SUBMIT</button>
     </form>
- <?php
+ <?php 
     $name = $_GET ['person'];
     echo $name." is the best teacher at madison!";
-    ?>
-  
-</body>
+    //This gets something from the database
 
+    /*echo "hi":*/
+    ?> 
+</body>
 </html>
